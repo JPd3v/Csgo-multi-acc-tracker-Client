0273ee7e87f173c6e40ddf8e71757cf05d67b547
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-03-03 21:17:07 -0300

    adds tailwind configuration and basic project
    dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,13 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
-
 function App() {
-  const [count, setCount] = useState(0);
-  const a = ['asd'];
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <div className="flex justify-center bg-sky-800 px-4 py-2  text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        <h1>Vite + React</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="flex justify-center bg-sky-800 px-4 py-2  text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        <h1>Vite + React</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </>
   );
 }
 
