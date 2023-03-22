647e2bf711c4bd5aea4d829e5475e589bc983446
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-03-22 09:25:54 -0300

    adds custom hook to edit account and modify
    the cache stored account if api call success

---
+++ src/features/steamAccounts/hooks/useEditAccount.tsx
@@ -0,0 +1,49 @@
+import {
+  InfiniteData,
+  useMutation,
+  useQueryClient,
+} from '@tanstack/react-query';
+import { axiosConfig } from 'config';
+import { IcreateAccount, IsteamAccount } from 'features/steamAccounts/types';
+import { IaxiosDefaultErrors } from 'types';
+
+interface ImutationParams {
+  accountId: string;
+  editedData: IcreateAccount;
+}
+
+async function editAccount(accountId: string, editedData: IcreateAccount) {
+  const req = await axiosConfig.put(`/steamAccounts/${accountId}`, editedData);
+
+  return req.data;
+}
+
+export default function useEditAccount() {
+  const queryclient = useQueryClient();
+
+  return useMutation<IsteamAccount, IaxiosDefaultErrors, ImutationParams>({
+    mutationFn: ({ accountId, editedData }) =>
+      editAccount(accountId, editedData),
+    onSuccess(data) {
+      queryclient.setQueryData<
+        InfiniteData<{
+          steamAccounts: IsteamAccount[];
+        }>
+      >(['steamAccounts'], (prev) => {
+        if (!prev) return undefined;
+        return {
+          ...prev,
+          pages: prev.pages.map((page) => ({
+            ...page,
+            steamAccounts: page.steamAccounts.map((account) => {
+              if (account._id === data._id) {
+                return { ...account, ...data };
+              }
+              return account;
+            }),
+          })),
+        };
+      });
+    },
+  });
+}
