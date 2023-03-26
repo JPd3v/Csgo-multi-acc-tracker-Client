505bbdb79f035a6769a023ea6736fda526a1c677
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-03-26 00:41:25 -0300

    adds infinite scroll to steam account lists
    -adds size prop to LoadingSpinner component

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -2,7 +2,8 @@ import { CgSpinner } from 'react-icons/cg';
 
 interface Iprops {
   className?: string;
+  size?: string | number | undefined;
 }
-export default function LoadingSpinner({ className }: Iprops) {
-  return <CgSpinner className={`animate-spin ${className}`} />;
+export default function LoadingSpinner({ className, size }: Iprops) {
+  return <CgSpinner className={`animate-spin ${className}`} size={size} />;
 }
--- src/features/steamAccounts/components/SteamAccountList.tsx
+++ src/features/steamAccounts/components/SteamAccountList.tsx
@@ -1,18 +1,50 @@
+import { LoadingSpinner } from 'components';
 import Account from 'features/steamAccounts/components/Account';
 import useSteamAccounts from 'features/steamAccounts/hooks/useSteamAccounts';
+import { useEffect, useRef } from 'react';
 
 export default function SteamAccountList() {
   const accountList = useSteamAccounts();
+  const loadingDivRef = useRef<HTMLDivElement>(null);
 
-  console.log(accountList.data?.pages);
+  useEffect(() => {
+    const intersectionObserver = new IntersectionObserver((entries) => {
+      if (entries[0].intersectionRatio <= 0) return;
+
+      if (accountList.hasNextPage) {
+        accountList.fetchNextPage();
+      }
+    });
+
+    if (loadingDivRef.current) {
+      intersectionObserver.observe(loadingDivRef.current);
+    }
+
+    return () => {
+      intersectionObserver.disconnect();
+    };
+  }, [loadingDivRef, accountList]);
 
   return (
-    <div>
+    <div className="mt-4 flex flex-col gap-3 overflow-auto p-1 sm:flex-row">
       {accountList.data?.pages.map((page) =>
         page.steamAccounts.map((account) => (
           <Account account={account} key={account._id} />
         ))
       )}
+
+      {accountList.hasNextPage ? (
+        <div
+          ref={loadingDivRef}
+          className="flex min-h-[4rem] items-center justify-center"
+        >
+          <LoadingSpinner size="3rem" />
+        </div>
+      ) : null}
+
+      {accountList.isError ? (
+        <p>Something went wrong getting your steam accounts</p>
+      ) : null}
     </div>
   );
 }
