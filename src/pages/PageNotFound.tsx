83755a8f9d71c187cd46c13c16a972bfaef30fe0
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-03-30 23:41:47 -0300

    fix bug which makes the pageNotFound and
    Suspense loading spinner over the navigation bar caused of
    position fixed css property

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -2,7 +2,7 @@ import { TbError404 } from 'react-icons/tb';
 
 export default function PageNotFound() {
   return (
-    <main className="fixed top-0 left-0 flex h-full w-full flex-col items-center justify-center">
+    <main className="absolute top-0 left-0 right-0 bottom-0 m-auto flex h-fit w-fit flex-col items-center justify-center">
       <TbError404 size="6rem" />
       <p className="text-xl font-bold">Page Not Found.</p>
     </main>
